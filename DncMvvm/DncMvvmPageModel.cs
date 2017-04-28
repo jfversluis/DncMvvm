7256925dd6a91895e5846429a8158d2eb025d401
Gerald Versluis <gerald@verslu.is>
2017-04-28 14:10:03 +0200

    Implemented INotifyPropertyChanged interface

--- DncMvvm/DncMvvmPageModel.cs
+++ DncMvvm/DncMvvmPageModel.cs
@@ -1,18 +1,34 @@
-﻿using Xamarin.Forms;
+﻿using System.ComponentModel;
+using Xamarin.Forms;
 
 namespace DncMvvm
 {
-	public class DncMvvmPageModel
+	public class DncMvvmPageModel : INotifyPropertyChanged
 	{
-		public string LabelText { get; set; }
+		private string _labelText;
+		public string LabelText
+		{
+			get
+			{
+				return _labelText;
+			}
+			set
+			{
+				_labelText = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LabelText)));
+			}
+		}
 
 		public Command KnockCommand { get; }
 
 		public DncMvvmPageModel()
 		{
-			KnockCommand = new Command(() => {
+			KnockCommand = new Command(() =>
+			{
 				LabelText = "Who's there?";
 			});
 		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
 	}
 }
