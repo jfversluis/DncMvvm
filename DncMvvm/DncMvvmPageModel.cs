5db26dfa6df1e6dbcfb7e856bb0f53c0f17e5be1
Gerald Versluis <gerald@verslu.is>
2017-04-28 13:44:23 +0200

    Implemented KnockCommand

--- DncMvvm/DncMvvmPageModel.cs
+++ DncMvvm/DncMvvmPageModel.cs
@@ -1,8 +1,18 @@
-﻿using System;
+﻿using Xamarin.Forms;
+
 namespace DncMvvm
 {
 	public class DncMvvmPageModel
 	{
 		public string LabelText { get; set; }
+
+		public Command KnockCommand { get; }
+
+		public DncMvvmPageModel()
+		{
+			KnockCommand = new Command(() => {
+				LabelText = "Who's there?";
+			});
+		}
 	}
 }
