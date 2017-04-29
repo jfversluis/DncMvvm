de27183f734fe08c09a74027f509c33735e49735
Gerald Versluis <gerald@verslu.is>
2017-04-29 10:49:47 +0200

    Implemented a IValueConverter

---
+++ DncMvvm/WhosThereToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace DncMvvm
+{
+	public class WhosThereToColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var stringValue = value as string;
+
+			if (string.IsNullOrWhiteSpace(stringValue))
+				return Color.Black;
+
+			if (stringValue.ToLowerInvariant().Contains("who"))
+				return Color.Red;
+
+			return Color.Black;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
